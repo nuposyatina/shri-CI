f9d6b732c77b2ecd657c5e281db35c6e19bd64da
Anna Tsukanova <nuposyatina@gmail.com>
2020-04-12 16:26:20 +0300

    Добавила тест-кейсы для модульных тестов

---
+++ __tests__/components/BuildHistory.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BuildHistory from 'layouts/Form';
+
+describe('Поведение компонента', () => {
+  test('При нажатии на кнопку «Run Build» появляется модальное окно', () => {
+
+  });
+
+  test('Кнопка «Show More» исчезает, если загружен первый билд', () => {
+
+  });
+
+  test('При нажатии на «Show More» происходит запрос списка билдов на сервер', () => {
+
+  });
+});
