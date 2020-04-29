c3aaa4a6d9d1909b8b9d57877164fa4951937df6
Anna Tsukanova <nuposyatina@gmail.com>
2020-04-29 20:59:41 +0300

    Добавила алиасы для tsconfig

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/configureStore';
-import { getSettings } from './store/actions/settings';
+import { store } from 'store/configureStore';
+import { getSettings } from 'store/actions/settings';
 import Application from './components/Application';
 
 store.dispatch(getSettings());
