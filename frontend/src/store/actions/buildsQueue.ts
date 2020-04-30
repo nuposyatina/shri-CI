8c1ff401bd032b1f203d011704511e429e1b0e59
Anna Tsukanova <nuposyatina@gmail.com>
2020-05-01 00:16:25 +0300

    Добавила типы в actions очереди билдов

--- frontend/src/store/actions/buildsQueue.ts
+++ frontend/src/store/actions/buildsQueue.ts
@@ -1,12 +1,28 @@
-const getQueryParams = ({ offset, limit }) => {
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { Build } from 'backend/server';
+
+export type LimitOffsetParams = {
+  limit: string | undefined,
+  offset: string | undefined
+}
+
+const getQueryParams = ({ offset, limit }: LimitOffsetParams) => {
   if (!offset && !limit) return '';
   if (!offset && limit) return `?limit=${limit}`;
   if (!limit && offset) return `?offset=${offset}`;
   return `?offset=${offset}&limit=${limit}`;
 };
 
-export const getBuilds = (params) => {
-  return (dispatch) => {
+const getBuildsSuccess = (builds: Build[]) => ({
+  type: 'GET_BUILDS_SUCCESS',
+  payload: {
+    builds
+  }
+});
+
+export const getBuilds = (params: LimitOffsetParams): ThunkAction<any, any, any, Action> => {
+  return (dispatch: ThunkDispatch<any, any, Action>) => {
     const query = getQueryParams(params);
     fetch(`http://localhost:3000/api/builds${query}`).
     then((response) => response.json()).
@@ -15,10 +31,3 @@ export const getBuilds = (params) => {
     })
   }
 };
-
-const getBuildsSuccess = (builds) => ({
-  type: 'GET_BUILDS_SUCCESS',
-  payload: {
-    builds
-  }
-});
