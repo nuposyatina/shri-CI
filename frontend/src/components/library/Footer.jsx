249a91e579ef94a52408cedcfb31ffd3d556c916
Anna Tsukanova <nuposyatina@gmail.com>
2020-05-24 13:43:11 +0300

    начала добавлять локализацию

--- frontend/src/components/library/Footer.jsx
+++ frontend/src/components/library/Footer.jsx
@@ -8,6 +8,7 @@ export default class Footer extends Component {
           <div className='Footer__Links'>
             <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>Support</a>
             <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>Learning</a>
+            <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>Русская версия</a>
           </div>
           <div className='Footer__Copyright Text Text_size_s Text_view_secondary'>© 2020 Your Name</div>
         </div>
