62a67eebb87e32ceb4e34be11e7c43db69f8b6bf
Anna Tsukanova <nuposyatina@gmail.com>
2020-05-25 22:42:32 +0300

    Добавила обработчик смены языка

--- frontend/src/components/library/Footer.jsx
+++ frontend/src/components/library/Footer.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'lib';
+import { changeCurrentLanguage, getLocales } from 'store/actions/locales';
 
-export default class Footer extends Component {
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeLanguage = this.onChangeLanguage.bind(this);
+  }
+  
+
+  onChangeLanguage(e) {
+    e.preventDefault();
+    const { locales, dispatch } = this.props;
+    const { currentLanguage } = locales;
+    const newLanguage = currentLanguage === 'en' ? 'ru' : 'en';
+    if (locales[newLanguage]) {
+      return dispatch(changeCurrentLanguage(newLanguage));
+    }
+
+    dispatch(getLocales(newLanguage));
+  }
+
   render() {
     return (
       <footer className='Footer'>
@@ -9,7 +29,11 @@ export default class Footer extends Component {
           <div className='Footer__Links'>
             <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>{ localize('Footer_SupportLink') }</a>
             <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>{ localize('Footer_LearningLink') }</a>
-            <a href='' className='Footer__Link Text Text_size_s Text_view_secondary'>{ localize('Footer_LangLink') }</a>
+            <a
+              href=''
+              onClick={ this.onChangeLanguage }
+              className='Footer__Link Text Text_size_s Text_view_secondary'
+            >{ localize('Footer_LangLink') }</a>
           </div>
           <div className='Footer__Copyright Text Text_size_s Text_view_secondary'>{ localize('Footer_Copyright') }</div>
         </div>
@@ -17,3 +41,9 @@ export default class Footer extends Component {
     )
   }
 }
+
+export default connect((state) => {
+  return {
+    locales: state.locales
+  };
+})(Footer);
